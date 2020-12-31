344d15226957acdfc415ac88692a4bc6b0ab237a
Fikri Mustofa <mustofa13.fikri@gmail.com>
2020-12-31 18:24:51 +0700

    Connect to database

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from Login.Login import Ui_Login as Login
 from Register.Register import Ui_Register as Register
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+import pymysql
 
 
 class WindowSelector:
@@ -24,7 +28,29 @@ class WindowSelector:
         self.show_window(window)
 
 
+def setupDb():
+    host = os.getenv("DB_HOST")
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("DB_PASSWORD")
+    database = os.getenv("DB_DATABASE")
+
+    try:
+        db = pymysql.connect(host, username, password, database)
+    except pymysql.MySQLError as e:
+        print(e)
+        return False
+
+    return db
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window_selector = WindowSelector()
-    sys.exit(app.exec_())
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
+    db = setupDb()
+
+    if not db == False:
+        window_selector = WindowSelector()
+        aex = app.exec_()
+        db.close()
+        sys.exit(aex)
